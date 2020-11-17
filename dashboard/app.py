3aea4e55939e633cf84e5df42268e2fc71682115
jethro <jethro.j.richards@gmail.com>
2020-11-17 12:21:54 +0800

    Feature/sysmon: Push

--- dashboard/app.py
+++ dashboard/app.py
@@ -8,7 +8,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-
+@app.route('/api/device-load')
+def DeviceLoad():
+    return render_template()
 @app.route('/about')
 def about():
     return render_template('about.html')
