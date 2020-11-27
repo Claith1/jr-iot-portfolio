328a4bfc9bf6102cc6fc0b4e453f02f85061c3fc
Claith1 <57791479+Claith1@users.noreply.github.com>
2020-11-28 01:39:04 +0800

    feature/environment: Environmental data monitoring and storage

--- monitor_ii/api.py
+++ monitor_ii/api.py
@@ -26,45 +26,38 @@ def chart_cpu():
 
 @app.route("/api/environment")
 def get_api_environment():
-    return {"error": "Route no implemented",
-            "temperature": None,
-            "pressure": None,
-            "humidity": None}
+        var = get_last_temperature(1)
+        temp = list(zip(*var))[0]
 
+        var = get_last_pressure(1)
+        pressure = list(zip(*var))[0]
+
+        var = get_last_humidity(1)
+        humidity = list(zip(*var))[0]
+        return {"temperature" : temp,"pressure":pressure,"humidity" : humidity}
 
 @app.route("/api/temperature/<count>")
 def get_api_temperature_multi(count =1):
-    var = get_last_temperature(int(count))
-    for ind in range(len(var)):
-        var[ind] = str(var[ind]).replace(',','')
-        var[ind] = var[ind].replace('(','')
-        var[ind] = float(var[ind].replace(')',''))
-    return {"temperature" : var}
+    var = get_last_temperature(count)
+    val = list(zip(*var))[0]
+    return {"temperature": val}
 
 @app.route("/api/humidity/<count>")
 def get_api_humidity_multi(count =1):
-    var = get_last_humidity(int(count))
-    for ind in range(len(var)):
-        var[ind] = str(var[ind]).replace(',','')
-        var[ind] = var[ind].replace('(','')
-        var[ind] = float(var[ind].replace(')',''))
-    return {"humidity" : var}
+    var = get_last_humidity(count)
+    val = list(zip(*var))[0]
+    return {"humidity": val}
 
 @app.route("/api/pressure/<count>")
 def get_api_pressure_multi(count =1):
-    var = get_last_pressure(int(count))
-    for ind in range(len(var)):
-        var[ind] = str(var[ind]).replace(',','')
-        var[ind] = var[ind].replace('(','')
-        var[ind] = float(var[ind].replace(')',''))
-    return {"pressure" : var}
+    var = get_last_pressure(count)
+    val = list(zip(*var))[0]
+    return {"pressure" : val}
 
 @app.route("/api/temperature")
 def get_api_temperature():
     var = get_last_temperature(1)
-    val = list(list(zip(*var))[0])
-    print(str(val))
-    print(var[0])
+    val = list(zip(*var))[0]
     return {"temperature" : val}
 
 
@@ -72,21 +65,15 @@ def get_api_temperature():
 @app.route("/api/pressure")
 def get_api_pressure():
     var = get_last_pressure(1)
-    for ind in range(len(var)):
-        var[ind] = str(var[ind]).replace(',', '')
-        var[ind] = var[ind].replace('(', '')
-        var[ind] = float(var[ind].replace(')', ''))
-    return {"pressure": var[0]}
+    val = list(zip(*var))[0]
+    return {"pressure": val}
 
 
 @app.route("/api/humidity")
 def get_api_humidity():
     var = get_last_humidity(1)
-    for ind in range(len(var)):
-        var[ind] = str(var[ind]).replace(',', '')
-        var[ind] = var[ind].replace('(', '')
-        var[ind] = float(var[ind].replace(')', ''))
-    return {"humidity": var[0]}
+    val = list(zip(*var))[0]
+    return {"humidity": val}
 
 
 if __name__ == '__main__':
